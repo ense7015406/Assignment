590824f53cdd24e9a4b8dbaf410f53072d042fa9
dtm3080 <harshilpatel3010@gmail.com>
2023-09-18 13:24:54 +1200

    updating fixes

--- speed-frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-export default function Home() {
-	return (
-		<main className={styles.main}>
-			<h1>Software Practice Empirical Evidence Database</h1>
-		</main>
-	);
-}
