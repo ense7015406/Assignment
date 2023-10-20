a97ece52d64a6eddd62cc74e9de4f53d8d1564c3
YakshaHarshil <harshilpatel3010@gmail.com>
2023-10-21 02:30:59 +1300

    Buttons add
    feat: On the moderator page I was able to get the buttons to show. There is one for add and one for delete. These button does nothing

--- frontend/src/pages/moderateViewPage.tsx
+++ frontend/src/pages/moderateViewPage.tsx
@@ -4,7 +4,6 @@ import SortableTable from "../components/table/SortableTable";
 import SearchArticles from "../components/searchBar/SearchBar";
 
 interface ArticlesInterface {
-	id: string;
 	title: string;
 	authors: string;
 	journal: string;
@@ -14,23 +13,27 @@ interface ArticlesInterface {
 	pubyear: string;
 	doi: string;
 	claim: string;
+	actions: string;
+	
 }
 type ArticlesProps = {
 	articles: ArticlesInterface[];
 };
 
+
 const Articles: NextPage<ArticlesProps> = ({ articles }) => {
 	const headers: { key: keyof ArticlesInterface; label: string }[] = [
-		{ key: "title", label: "Title" },
-		{ key: "authors", label: "Authors" },
-		{ key: "journal", label: "Journal" },
-		{ key: "volume", label: "Volume" },
-		{ key: "number", label: "Number" },
-		{ key: "pages", label: "Pages" },
-		{ key: "pubyear", label: "Publication Year" },
-		{ key: "doi", label: "DOI" },
-		{ key: "claim", label: "Claim" },
-	];
+	{ key: "title", label: "Title" },
+	{ key: "authors", label: "Authors" },
+	{ key: "journal", label: "Journal" },
+	{ key: "volume", label: "Volume" },
+	{ key: "number", label: "Number" },
+	{ key: "pages", label: "Pages" },
+	{ key: "pubyear", label: "Publication Year" },
+	{ key: "doi", label: "DOI" },
+	{ key: "claim", label: "Claim" },
+	{ key: "actions", label: "Actions" }, // "actions" is a valid key
+  ];
 
 	const [searchResults, setSearchResults] = useState<ArticlesProps[]>([]);
 
@@ -46,18 +49,43 @@ const Articles: NextPage<ArticlesProps> = ({ articles }) => {
 		}
 	};
 
+	// Function to add an article to the main database
+	const handleAddArticle = async () => {
+		
+	  };
+  
+	// Function to delete an article by ID
+	const handleDeleteArticle = async () => {
+		
+	};
+
+
 	return (
 		<div className="container">
-			<h1>Moderater View</h1>
-
-			<div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
-				<p>Page containing a table of articles:</p>
-				<SearchArticles onSearch={handleSearch} />
-			</div>
-			<SortableTable headers={headers} data={searchResults.length > 0 ? searchResults : articles} />
+		  <h1>Moderator View</h1>
+		  <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+			<p>Page containing a table of articles:</p>
+			<SearchArticles onSearch={handleSearch} />
+		  </div>
+		  <SortableTable
+			headers={headers}
+			data={searchResults.length > 0 ? searchResults : articles}
+			// Render Add and Delete buttons in the last column
+			renderCell={(article, key) => {
+			  if (key === "actions") {
+				return (
+				  <div>
+					<button onClick={() => handleAddArticle()}>Add</button>
+					<button onClick={() => handleDeleteArticle()}>Delete</button>
+				  </div>
+				);
+			  }
+			  return article[key];
+			}}
+		  />
 		</div>
-	);
-};
+	  );
+	};
 
 export const getServerSideProps: GetServerSideProps<ArticlesProps> = async () => {
 	try {
